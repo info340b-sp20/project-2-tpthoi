e01aad7acc97def8306018a4ca1c1ee3202d76c2
pragynanaik <pragyna@uw.edu>
2020-06-01 09:47:52 -0700

    adding config

---
+++ src/Config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4MExs19u_DIxV-fvX3FlS99jfOt_p7jE",
+    authDomain: "mycloset-5a151.firebaseapp.com",
+    databaseURL: "https://mycloset-5a151.firebaseio.com",
+    projectId: "mycloset-5a151",
+    storageBucket: "mycloset-5a151.appspot.com",
+    messagingSenderId: "732540682433",
+    appId: "1:732540682433:web:c7c48f43268d960f929440",
+    measurementId: "G-7PG0P9QCNG"
+  };
+
+  export default firebaseConfig;
